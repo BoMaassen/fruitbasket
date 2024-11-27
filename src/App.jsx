f4e45a00592c1651cf13c2f8a62216ede480c388
BoMaasen <bomaassen@live.nl>
2024-11-27 14:40:11 +0100

    opdracht 1 af

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,51 @@ import './App.css'
 import {useState} from "react";
 
 function App() {
-    const [fruitAmount, setFruitAmount] = useState(0);
-    console.log(fruitAmount)
-    return (<>        <h1>Fruitmand bezorgservice</h1>
+    let [fruitAardbeiAmount, setAardbeiFruitAmount] = useState(0);
+    let [fruitBanaanAmount, setBanaanFruitAmount] = useState(0);
+    let [fruitAppelAmount, setAppelFruitAmount] = useState(0);
+    let [fruitKiwiAmount, setKiwiFruitAmount] = useState(0);
+
+    function resetFruitCounter(){
+        return setAardbeiFruitAmount(fruitAardbeiAmount = 0) + setBanaanFruitAmount(fruitBanaanAmount = 0)
+        + setAppelFruitAmount(fruitAppelAmount = 0) + setKiwiFruitAmount(fruitKiwiAmount = 0)
+    }
+
+    return (<>
+        <h1>Fruitmand bezorgservice</h1>
         <section>
-            <article className="fruit-container"><h2>Aardbeiden</h2>
-                <button type="button" onClick={() => setFruitAmount(fruitAmount - 1)}>-</button>
-                <p>{fruitAmount}</p>
-                <button type="button" onClick={() => setFruitAmount(fruitAmount + 1)}>+</button>
+            <article className="fruit-container">
+                <h2>🍓 Aardbeiden</h2>
+                <button type="button" onClick={() => setAardbeiFruitAmount((prevCounter) => Math.max(prevCounter - 1, 0))}>-</button>
+                <p>{fruitAardbeiAmount}</p>
+                <button type="button" onClick={() => setAardbeiFruitAmount(fruitAardbeiAmount + 1)}>+</button>
             </article>
-            <article><h2>Bananen</h2>
-                <button type="button">-</button>
-                <button type="button">+</button>
+            <article className="fruit-container">
+                <h2> 🍌 Bananen</h2>
+                <button type="button"
+                        onClick={() => setBanaanFruitAmount((prevCounter) => Math.max(prevCounter - 1, 0))}>-
+                </button>
+                <p>{fruitBanaanAmount}</p>
+                <button type="button" onClick={() => setBanaanFruitAmount(fruitBanaanAmount + 1)}>+</button>
             </article>
-            <article><h2>Appels</h2>
-                <button type="button">-</button>
-                <button type="button">+</button>
+            <article className="fruit-container">
+                <h2>🍏 Appels</h2>
+                <button type="button"
+                        onClick={() => setAppelFruitAmount((prevCounter) => Math.max(prevCounter - 1, 0))}>-
+                </button>
+                <p>{fruitAppelAmount}</p>
+                <button type="button" onClick={() => setAppelFruitAmount(fruitAppelAmount + 1)}>+</button>
             </article>
-            <article><h2>Kiwi</h2>
-                <button type="button">-</button>
-                <button type="button">+</button>
+            <article className="fruit-container">
+                <h2>🥝 Kiwi&#39;s</h2>
+                <button type="button"
+                        onClick={() => setKiwiFruitAmount((prevCounter) => Math.max(prevCounter - 1, 0))}>-
+                </button>
+                <p>{fruitKiwiAmount}</p>
+                <button type="button" onClick={() => setKiwiFruitAmount(fruitKiwiAmount + 1)}>+</button>
             </article>
+
+            <button type="button" onClick={resetFruitCounter}>Reset</button>
         </section>
     </>)
 }
