2142219e5da98eb8131181ba667abcf252373097
BoMaassen <bomaassen@live.nl>
2024-11-28 11:18:23 +0100

    formulier af

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,11 @@ function App() {
     const [firstNameValue, setFirstNameValue] = useState('');
     const [lastNameValue, setLastNameValue] = useState('');
     const [ageValue, setAgeValue] = useState(0);
-    const [zipCodeValue, setzipCodeValue] = useState("");
+    const [zipCodeValue, setZipCodeValue] = useState("");
     const [delivery, setDelivery] = useState("");
     const [deliveryTime, setDeliveryTime] = useState("");
-    console.log(delivery);
+    const [comments, setComments] = useState("");
+    const [terms, setTerms] = useState(false);
 
     return (<>
         <h1>Fruitmand bezorgservice</h1>
@@ -60,28 +61,46 @@ function App() {
         </section>
         <section>
             <form className="form-container">
-                <label htmlFor="firstNameField">Voornaam:</label>
-                <input id="firstNameField" name="firstName" type="text" value={firstNameValue}
-                       onChange={(e) => setFirstNameValue(e.target.value)}/>
-                <label htmlFor="lastNameField">Achternaam:</label>
-                <input id="lastNameField" name="lastName" type="text" value={lastNameValue}
-                       onChange={(e) => setLastNameValue(e.target.value)}/>
-                <label htmlFor="ageField">Leeftijd:</label>
-                <input id="ageField" name="age" type="number" value={ageValue}
-                       onChange={(e) => setAgeValue(e.target.value)}/>
-                <label htmlFor="zipCodeField">Postcode:</label>
-                <input id="zipCodeField" name="zipCode" type="text" value={zipCodeValue}
-                       onChange={(e) => setzipCodeValue(e.target.value)}/>
-                <label htmlFor="delivery">Bezorgfrequentie</label>
-                <select id="delivery" name="delivery" value={delivery} onChange={(e) => setDelivery(e.target.value)}>
-                    <option value="weekly">iedere week</option>
-                    <option value="two-weekly">om de week</option>
-                    <option value="montly">iedere maand</option>
-                </select>
-                <input id="dayTime" name="deliveryTime" type="radio" checked value="dayTime" onChange={(e) => setDelivery(e.target.value)}/>
-                <label htmlFor="dayTime">Overdag</label>
-                <input id="evening" name="deliveryTime" type="radio" value="evening"  onChange={(e) => setDelivery(e.target.value)}  />
-                <label htmlFor="evening">&#39;s Avonds</label>
+                <label htmlFor="firstNameField">Voornaam:
+                    <input id="firstNameField" name="firstName" type="text" value={firstNameValue}
+                           onChange={(e) => setFirstNameValue(e.target.value)}/>
+                </label>
+                <label htmlFor="lastNameField">Achternaam:
+                    <input id="lastNameField" name="lastName" type="text" value={lastNameValue}
+                           onChange={(e) => setLastNameValue(e.target.value)}/>
+                </label>
+                <label htmlFor="ageField">Leeftijd:
+                    <input id="ageField" name="age" type="number" value={ageValue}
+                           onChange={(e) => setAgeValue(parseInt(e.target.value))}/>
+                </label>
+                <label htmlFor="zipCodeField">Postcode:
+                    <input id="zipCodeField" name="zipCode" type="text" value={zipCodeValue}
+                           onChange={(e) => setZipCodeValue(e.target.value)}/>
+                </label>
+                <label htmlFor="delivery">Bezorgfrequentie
+                    <select id="delivery" name="delivery" value={delivery}
+                            onChange={(e) => setDelivery(e.target.value)}>
+                        <option value="weekly">iedere week</option>
+                        <option value="two-weekly">om de week</option>
+                        <option value="montly">iedere maand</option>
+                    </select>
+                </label>
+                <label htmlFor="dayTime">
+                    <input id="dayTime" name="deliveryTime" type="radio" defaultChecked value={deliveryTime}
+                           onClick={() => setDeliveryTime("dayTime")}/>
+                    Overdag</label>
+                <label htmlFor="evening">
+                    <input id="evening" name="deliveryTime" type="radio" value={deliveryTime}
+                           onClick={() => setDeliveryTime("evening")}/>
+                    &#39;s Avonds</label>
+                <label htmlFor="comments">Opmerking
+                    <textarea id="comments" name="comments" value={comments}
+                              onChange={(e) => setComments(e.target.value)}></textarea>
+                </label>
+                <label htmlFor="terms">
+                    <input id="terms" name="terms" type="checkbox" checked={terms}
+                           onClick={() => setTerms(!terms)}/>
+                    Ik ga akkoord met de voorwaarden</label>
                 <button type="submit">Verzend</button>
             </form>
         </section>
