4487ac5cd8aad39b187c69fd1bd23ea0a6ad6149
BoMaassen <bomaassen@live.nl>
2024-11-29 11:46:46 +0100

    form met een onchange handler geschreven

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,33 @@ function App() {
     const [fruitBanaanAmount, setBanaanFruitAmount] = useState(0);
     const [fruitAppelAmount, setAppelFruitAmount] = useState(0);
     const [fruitKiwiAmount, setKiwiFruitAmount] = useState(0);
-    /*const [firstNameValue, setFirstNameValue] = useState("");*/
-    //const [lastNameValue, setLastNameValue] = useState("");
-    const [ageValue, setAgeValue] = useState(0);
-    const [zipCodeValue, setZipCodeValue] = useState("");
-    const [delivery, setDelivery] = useState("");
-    const [deliveryTime, setDeliveryTime] = useState("");
-    const [comments, setComments] = useState("");
-    const [terms, setTerms] = useState(false);
 
     const [formState, setFormState] = useState({
         firstName: "",
         lastName: "",
         age: 0,
+        zipCode: "",
+        delivery: "",
+        deliveryTime: "dayTime",
+        comments: "",
+        terms: false,
     });
 
     function handleChange(e){
        const changedField = e.target.name;
+       const newValue = e.target.type === "checkbox" ? e.target.checked : e.target.value
 
-        setFormState({
-            ...formState,
-        [changedField]: e.target.value,
-        });
-    }
+            setFormState({
+                ...formState,
+                [changedField]: newValue,
+            });
+        }
 
     function resetFruitCounter() {
-        return setAardbeiFruitAmount(0) + setBanaanFruitAmount(0)
-            + setAppelFruitAmount(0) + setKiwiFruitAmount(0);
+        return setAardbeiFruitAmount(0) + setBanaanFruitAmount(0) + setAppelFruitAmount(0) + setKiwiFruitAmount(0)
     }
 
     return (<>
-        <h1>Fruitmand bezorgservice</h1>
         <section>
             <article className="fruit-container">
                 <h2>🍓 Aardbeiden</h2>
@@ -86,34 +82,36 @@ function App() {
                            onChange={handleChange}/>
                 </label>
                 <label htmlFor="zipCodeField">Postcode:
-                    <input id="zipCodeField" name="zipCode" type="text" value={zipCodeValue}
-                           onChange={(e) => setZipCodeValue(e.target.value)}/>
+                    <input id="zipCodeField" name="zipCode" type="text" value={formState.zipCode}
+                           onChange={handleChange}/>
                 </label>
                 <label htmlFor="delivery">Bezorgfrequentie
-                    <select id="delivery" name="delivery" value={delivery}
-                            onChange={(e) => setDelivery(e.target.value)}>
+                    <select id="delivery" name="delivery" value={formState.delivery}
+                            onChange={handleChange}>
                         <option value="weekly">iedere week</option>
                         <option value="two-weekly">om de week</option>
                         <option value="montly">iedere maand</option>
                     </select>
                 </label>
                 <label htmlFor="dayTime">
-                    <input id="dayTime" name="deliveryTime" type="radio" defaultChecked value={deliveryTime}
-                           onClick={() => setDeliveryTime("dayTime")}/>
+                    <input id="dayTime" name="deliveryTime" type="radio" checked={formState.deliveryTime === "dayTime"}
+                           value="dayTime"
+                           onChange={handleChange}/>
                     Overdag</label>
 
                 <label htmlFor="evening">
-                    <input id="evening" name="deliveryTime" type="radio" value={deliveryTime}
-                           onClick={() => setDeliveryTime("evening")}/>
+                    <input id="evening" name="deliveryTime" type="radio" checked={formState.deliveryTime === "evening"}
+                           value={formState.deliveryTime}
+                           onChange={handleChange}/>
                     &#39;s Avonds
                 </label>
                 <label htmlFor="comments">Opmerking
-                    <textarea id="comments" name="comments" value={comments}
-                              onChange={(e) => setComments(e.target.value)}></textarea>
+                    <textarea id="comments" name="comments" value={formState.comments}
+                              onChange={handleChange}></textarea>
                 </label>
                 <label htmlFor="terms">
-                    <input id="terms" name="terms" type="checkbox" defaultChecked={terms}
-                           onClick={() => setTerms(!terms)}/>
+                    <input id="terms" name="terms" type="checkbox" defaultChecked={formState.terms}
+                           onChange={handleChange}/>
                     Ik ga akkoord met de voorwaarden
                 </label>
                 <button type="submit">Verzend</button>
